798bd4ff8d85645e2838ae115df2baaa2d16ed67
Krizel Rika <krizelcodes@gmail.com>
2025-11-11 21:22:14 +0800

    constructor

---
+++ hashmap.js
@@ -0,0 +1,10 @@
+class HashMap {
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.capacity = initialCapacity;
+    this.loadFactor = loadFactor;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.sizeCount = 0;
+  }
+
+  
+}
