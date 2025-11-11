b2d49c8f4c5d76a0c688aa2fd6ecd5189862acc8
Krizel Rika <krizelcodes@gmail.com>
2025-11-11 21:23:43 +0800

    hash

--- hashmap.js
+++ hashmap.js
@@ -6,5 +6,15 @@ class HashMap {
     this.sizeCount = 0;
   }
 
-  
+  // 🔢 Hash function — keeps values within integer range using modulo inside loop
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
+
+
 }
